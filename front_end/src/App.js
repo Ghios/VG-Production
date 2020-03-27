32c1396d3b4d81743752d73f748cb522883cd6ca
Nefito <vkorzhov00@gmail.com>
2020-03-27 16:23:18 +0200

    fix errors

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Searchbar from './containers/Searchbar/Searchbar.js';
 
-function App() {
-  return (
-    <div className="App">
-        <Searchbar/>
-    </div>
-  );
+class App extends Component {
+    render(){
+      return (
+        <div className="App">
+            <Searchbar/>
+        </div>
+      );
+  };
 }
 
 export default App;
--- front_end/src/containers/Searchbar/Searchbar.js
+++ front_end/src/containers/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import './Searchbar.css';
 
 class Searchbar extends React.Component {
