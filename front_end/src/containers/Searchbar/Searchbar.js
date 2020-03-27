554476bc6e3ef92b4ee6f976ed0e24f6c83e1de2
Nefito <vkorzhov00@gmail.com>
2020-03-27 16:12:45 +0200

    add closing tag to <input>

--- front_end/src/containers/Searchbar/Searchbar.js
+++ front_end/src/containers/Searchbar/Searchbar.js
@@ -6,7 +6,7 @@ class Searchbar extends React.Component {
         return(
             <nav className="navbar navbar-light bg-light fixed-top">
                 <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                     <button className="btn my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </nav>
