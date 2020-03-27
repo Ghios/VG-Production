97adc6ac639955c656fe19a0dc5b0c4cc906dc77
Nefito <vkorzhov00@gmail.com>
2020-03-27 16:07:19 +0200

    add searchbar

---
+++ front_end/src/containers/Searchbar/Searchbar.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import './Searchbar.css';
+
+class Searchbar extends React.Component {
+    render() {
+        return(
+            <nav className="navbar navbar-light bg-light fixed-top">
+                <form className="form-inline">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button className="btn my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </nav>
+        );
+    }
+}
+export default Searchbar;
