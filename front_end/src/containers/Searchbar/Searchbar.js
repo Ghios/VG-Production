8c4533b64479ca5dd694ec97a0e4629d73d8e3dc
Nefito <vkorzhov00@gmail.com>
2020-03-27 17:55:41 +0200

    add radio buttons

--- front_end/src/containers/Searchbar/Searchbar.js
+++ front_end/src/containers/Searchbar/Searchbar.js
@@ -5,10 +5,18 @@ class Searchbar extends React.Component {
     render() {
         return(
             <nav className="navbar navbar-light bg-light fixed-top">
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn my-2 my-sm-0" type="submit">Search</button>
+                <form className="form-inline mx-auto">
+                    <input className="form-control mr-sm-2 border-warning shadow-none" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button className="btn my-2 my-sm-0 btn-outline-warning" type="submit">Search</button>
                 </form>
+                <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label className="btn btn-warning active">By Recipe
+                        <input type="radio" name="options" id="byRecipe" checked></input>
+                    </label>
+                    <label className="btn btn-warning">
+                        <input type="radio" name="options" id="byIngridient"></input>By Ingridients
+                    </label>
+                </div>
             </nav>
         );
     }
